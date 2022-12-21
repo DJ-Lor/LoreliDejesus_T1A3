55d273134b73d799e39dac81d709c22acbb1ed6f
Loreli <dejesus.loreli@gmail.com>
2022-12-22 09:48:12 +1100

    Added linter pkg pycodestyleEnabled

--- input_helper.py
+++ input_helper.py
@@ -1,8 +1,10 @@
 from email_validator import validate_email, EmailNotValidError
- 
-# Input ID 
+
+# Input ID
+
+
 def capture_id():
-    
+
     while True:
         try:
             input_id = int(input('Enter ID: '))
@@ -20,7 +22,7 @@ def capture_name():
     input_name = input_name.lower()
     while (len(input_name) <= 1):
         print('Invalid name. Please try again.')
-        input_name = input('Please enter the new client name: ') 
+        input_name = input('Please enter the new client name: ')
         input_name = input_name.lower()
     return input_name
 
@@ -31,7 +33,7 @@ def check_email(email):
       # validate and get info
         v = validate_email(email)
         # replace with normalized form
-        email = v['email'] 
+        email = v['email']
         return True
     except EmailNotValidError as e:
         # email is not valid, exception message is human-readable
@@ -39,9 +41,11 @@ def check_email(email):
         return False
 
 # Input email function and error handling
+
+
 def capture_email():
     input_email = input('Please enter the client email address: ')
-    while(check_email(input_email) == False) :
+    while (check_email(input_email) == False):
         print('Invalid email. Please try again.')
         input_email = input('Please enter the client email address: ')
     return input_email
@@ -54,17 +58,17 @@ def capture_suburb():
     while (len(input_suburb) <= 3):
         print('Invalid name. Please try again.')
         input_suburb = input('Please enter the suburb name: ')
-        input_suburb = input_suburb.lower()    
+        input_suburb = input_suburb.lower()
     return input_suburb
-   
 
 
 # Input price function and error handling
 def capture_price():
-    
+
     while True:
         try:
-            input_price = int(input('Please enter the property price/budget: '))
+            input_price = int(
+                input('Please enter the property price/budget: '))
             break
 
         except ValueError:
@@ -73,19 +77,17 @@ def capture_price():
     return input_price
 
 
-# Input deletion confirmation and error handling 
+# Input deletion confirmation and error handling
 
 def capture_confirm_action():
     while True:
-        confirmed_action = input(f'Are you sure you want to delete above ID from the list? Y/N: ')
+        confirmed_action = input(
+            f'Are you sure you want to delete above ID from the list? Y/N: ')
         confirmed_action = confirmed_action.capitalize()
-      
+
         if confirmed_action == 'Y' or confirmed_action == 'N':
             break
         else:
             print('Invalid input. Choose between Y or N. Try again')
 
     return confirmed_action
-
- 
-         
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from helper import id_display, next_step
 from input_helper import capture_name, capture_email, capture_suburb, capture_price, capture_id, capture_confirm_action
 
 
-
 user_input = ''
 
 while True:
@@ -15,7 +14,8 @@ while True:
 
     # Manage Property option
     if user_input == '1':
-        manage_prop_choice = input('What would you like to do?\n 1. Add a new property to list\n 2. Edit a current property on the list\n 3. Delete a property on the list\n 4. Exit\n Please enter a number: ')
+        manage_prop_choice = input(
+            'What would you like to do?\n 1. Add a new property to list\n 2. Edit a current property on the list\n 3. Delete a property on the list\n 4. Exit\n Please enter a number: ')
         manage_prop_choice = manage_prop_choice.strip()
         os.system('cls||clear')
 
@@ -27,11 +27,11 @@ while True:
             p1 = Property(suburb, price)
             p1.property_save()
             os.system('cls||clear')
-            print(f'The new property located at {suburb} with a selling price of ${price} has been added!')
+            print(
+                f'The new property located at {suburb} with a selling price of ${price} has been added!')
             print(f'Your property ID is {id_display()}!')
             next_step()
             os.system('cls||clear')
-            
 
     # Edit a current Property from the list
         elif manage_prop_choice == '2':
@@ -40,8 +40,8 @@ while True:
             p3.property_edit()
             next_step()
             os.system('cls||clear')
-            
-    # Delete a current Property from the list 
+
+    # Delete a current Property from the list
         elif manage_prop_choice == '3':
             id = capture_id()
             confirm_action = capture_confirm_action()
@@ -53,9 +53,10 @@ while True:
                 os.system('cls||clear')
 
             if confirm_action == 'N':
-                print('No property data has been deleted. Please choose again from the following options')
+                print(
+                    'No property data has been deleted. Please choose again from the following options')
                 continue
-    
+
     # Exit option
         elif manage_prop_choice == '4':
             print('Good bye')
@@ -67,11 +68,12 @@ while True:
 
     elif user_input == '2':
         user_input = user_input.strip()
-        manage_client_choice = input('What would you like to do?\n 1. Add a new client\n 2. Edit current client details on the list\n 3. Delete a client on the list\n 4. Exit\n Please enter a number: ')
+        manage_client_choice = input(
+            'What would you like to do?\n 1. Add a new client\n 2. Edit current client details on the list\n 3. Delete a client on the list\n 4. Exit\n Please enter a number: ')
         manage_client_choice = manage_client_choice.strip()
         os.system('cls||clear')
 
-    # Add a new client to the list 
+    # Add a new client to the list
         if manage_client_choice == '1':
             name = capture_name()
             email = capture_email()
@@ -81,7 +83,8 @@ while True:
             c1 = Client(name, email, suburb, price)
             c1.client_save()
             os.system('cls||clear')
-            print(f'New client added!\n Client name: {name}\n Email: {email}\n Looking for property in: {suburb}\n Budget: ${price}\n Your client ID is {id_display()}!')
+            print(
+                f'New client added!\n Client name: {name}\n Email: {email}\n Looking for property in: {suburb}\n Budget: ${price}\n Your client ID is {id_display()}!')
             print('If this is incorrect, please go back to the main portal and follow the prompts to edit client details.')
             next_step()
             os.system('cls||clear')
@@ -89,26 +92,26 @@ while True:
     # Edit client details
         elif manage_client_choice == '2':
             id = capture_id()
-            c3 = Client('','','', 0, id)
+            c3 = Client('', '', '', 0, id)
             c3.client_edit()
             next_step()
             os.system('cls||clear')
-            
-    # Delete client details 
+
+    # Delete client details
         elif manage_client_choice == '3':
             id = capture_id()
             confirm_action = capture_confirm_action()
-            
+
             if confirm_action == 'Y':
-                c2 = Client('','','', 0, id)
+                c2 = Client('', '', '', 0, id)
                 c2.client_remove()
                 next_step()
                 os.system('cls||clear')
-            
-            elif confirm_action == 'N':
-                print('No client data has been deleted. Please choose again from the following options.')
-                continue
 
+            elif confirm_action == 'N':
+                print(
+                    'No client data has been deleted. Please choose again from the following options.')
+                continue
 
     # Exit option
 
@@ -120,21 +123,22 @@ while True:
 
     # Prospective opportunities view for client and property owners
     elif user_input == '3':
-        opp_portal_choice = input('Check out prospective business opportunities. Please select preferred view:\n 1. Client ID\n 2. Property ID\n Enter here: ')
+        opp_portal_choice = input(
+            'Check out prospective business opportunities. Please select preferred view:\n 1. Client ID\n 2. Property ID\n Enter here: ')
         if opp_portal_choice == '1':
             id = capture_id()
             c4 = Client('', '', '', 0, id)
             c4.client_search()
             next_step()
             os.system('cls||clear')
-            
+
         elif opp_portal_choice == '2':
             id = capture_id()
             p4 = Property('', 0, id)
             p4.property_search()
             next_step()
             os.system('cls||clear')
-            
+
         else:
             print('Invalid input. Please choose again.')
 
@@ -144,4 +148,3 @@ while True:
 
     else:
         print('Invalid input. Please choose a number from 1-4: ')
-
