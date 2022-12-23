ce98fca26ab0adf7eec16485094dde1516a2a4c7
Loreli <dejesus.loreli@gmail.com>
2022-12-23 15:46:17 +1100

    bash script python version check edited

--- run_app.sh
+++ run_app.sh
@@ -4,7 +4,13 @@ if [[ -x "$(command -v python)" ]]
 then
     pyv="$(python -V 2>&1)"
     py3v="$(python3 -V 2>&1)"
-    if [[ $pyv == "Python 3"* || $py3v == "Python 3"* ]]
+    if [[ $pyv == "Python 3"* ]]
+    then
+        python -m venv venv
+        source venv/bin/activate
+        pip install email_validator
+        python ./main.py
+    elif [[ $py3v == "Python 3"* ]]
     then
         python3 -m venv venv
         source venv/bin/activate
